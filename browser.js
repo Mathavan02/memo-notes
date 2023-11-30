8e12b8f3015d66a54d7df56b4a251bc5cee63e4d
Mathavan02 <152476322+Mathavan02@users.noreply.github.com>
2023-11-30 13:44:46 +0530

    Add files via upload

---
+++ browser.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("click",function(e){
+
+    //update
+    if (e.target.classList.contains("btn-warning")) {
+        e.preventDefault();
+        
+        // Use an input field for updating the value
+        const newValue = prompt("Enter New Value: ");
+        
+        // Check if the user entered a value
+        if (newValue !== null) {
+            axios.post('/update-item', { text: newValue, id: e.target.getAttribute("data-id") }).then(function () {
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = newValue;
+            }).catch(function () {
+                console.log("error, try again!");
+            });
+        }
+    }
+
+    //delete
+    if (e.target.classList.contains("btn-danger")) {
+        e.preventDefault();
+
+        // Use window.confirm for user confirmation
+        if (window.confirm("Do you really want to Delete it")) {
+            axios.post('/delete-item', { id: e.target.getAttribute("data-id") }).then(function () {
+                e.target.parentElement.parentElement.remove();
+            }).catch(function () {
+                console.log("error, try again!");
+            });
+        }
+    }
+
+})
